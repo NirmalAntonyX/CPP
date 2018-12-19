abf4ce966c0525d49c5aac5e89cc52e717eef96e
Nirmal Antony X <nirmalx@juniper.net>
2018-12-19 13:01:56 +0530

    indent

--- produce_consumer.cpp
+++ produce_consumer.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
-#include<thread>
-#include<vector>
-#include<queue>
-#include<algorithm>
-#include<condition_variable>
-#include<atomic>
-
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <condition_variable>
+#include <atomic>
 
 using namespace std;
 
@@ -19,110 +18,111 @@ const int num_of_items_to_produce = 5;
 const int num_of_items_to_consume = 3;
 queue<int> queue_buffer;
 
-
-class Producer 
+class Producer
 {
     int item;
-    public:
-   Producer() : item(0){
-       
-   }
 
-   void thread_run(int thread_id)
-   {
-      cout<<"Producer thread run"<<endl;
-     
-      
-      // wait if buffer is full.
-     while (1) {
-         cout<<"queue size " << queue_buffer.size() <<endl;
-           
-         {
-           //  queue_buffer.push(1);
-           unique_lock<mutex> lock_guard(m_mutex); 
-      full_cond_var.wait_for(lock_guard,  chrono::seconds(2), [] {
-                                     return queue_buffer.size() <= max_num_of_items;
-                                    }
-                    );
-cout<<"PRODUCER THREAD "<<endl;
-      // out of blocked state. Lets do some job now.
-      for (int i=0;i<num_of_items_to_consume;i++) {
-          cout<<"Producing element "<<item<<" iter " << i <<endl;
-          item++;
-          if (item >= max_num_of_items) {
-              break;
-          }
-           queue_buffer.push(item);
-           }
+  public:
+    Producer() : item(0)
+    {
+    }
 
+    void thread_run(int thread_id)
+    {
+        cout << "Producer thread run" << endl;
 
-      lock_guard.unlock();
-    empty_cond_var.notify_all();  // have produced some items. Consumers please start consuming.
-   // this_thread::sleep_for(chrono::milliseconds(500));
-   }
- //empty_cond_var.notify_all();  // have produced some items. Consumers please start consuming.
-   this_thread::sleep_for(chrono::milliseconds(500));
- //    this_thread::yield();
-     }
+        // wait if buffer is full.
+        while (1)
+        {
+            cout << "queue size " << queue_buffer.size() << endl;
 
-   }
+            {
+                //  queue_buffer.push(1);
+                unique_lock<mutex> lock_guard(m_mutex);
+                full_cond_var.wait_for(lock_guard, chrono::seconds(2), [] {
+                    return queue_buffer.size() <= max_num_of_items;
+                });
+                cout << "PRODUCER THREAD " << endl;
+                // out of blocked state. Lets do some job now.
+                for (int i = 0; i < num_of_items_to_consume; i++)
+                {
+                    cout << "Producing element " << item << " iter " << i << endl;
+                    item++;
+                    if (item >= max_num_of_items)
+                    {
+                        break;
+                    }
+                    queue_buffer.push(item);
+                }
 
+                lock_guard.unlock();
+                empty_cond_var.notify_all(); // have produced some items. Consumers please start consuming.
+                // this_thread::sleep_for(chrono::milliseconds(500));
+            }
+            //empty_cond_var.notify_all();  // have produced some items. Consumers please start consuming.
+            this_thread::sleep_for(chrono::milliseconds(500));
+            //    this_thread::yield();
+        }
+    }
 };
 
 class Consumer
 {
-   public:
-    Consumer(){}
-    
-   void thread_run(int thread_id)
-   {
-      cout<<"Consumer thread run"<<endl;
+  public:
+    Consumer() {}
 
+    void thread_run(int thread_id)
+    {
+        cout << "Consumer thread run" << endl;
 
-      // wait if buffer is empty.
-     while (1) {
-        
-         {
-         unique_lock<mutex> lock_guard(m_mutex);           
-      empty_cond_var.wait_for(lock_guard, chrono::seconds(2), [] {
-                                     return  !queue_buffer.empty();
-                                    }
-                    );
-cout<<"Consumer THREAD "<<endl;
-        for (int i=0;i<num_of_items_to_consume;i++) {
-            if (queue_buffer.size() > 0) {
-             int consumed_element =  queue_buffer.front();
-             cout<<"Consumed element "<<consumed_element<<endl;
-             queue_buffer.pop();
+        // wait if buffer is empty.
+        while (1)
+        {
+
+            {
+                unique_lock<mutex> lock_guard(m_mutex);
+                empty_cond_var.wait_for(lock_guard, chrono::seconds(2), [] {
+                    return !queue_buffer.empty();
+                });
+                cout << "Consumer THREAD " << endl;
+                for (int i = 0; i < num_of_items_to_consume; i++)
+                {
+                    if (queue_buffer.size() > 0)
+                    {
+                        int consumed_element = queue_buffer.front();
+                        cout << "Consumed element " << consumed_element << endl;
+                        queue_buffer.pop();
+                    }
+                }
+                lock_guard.unlock();
+                full_cond_var.notify_all(); // some space is there. producer please produce.
+                //this_thread::sleep_for(chrono::milliseconds(500));
+            }
+            //full_cond_var.notify_all(); // some space is there. producer please produce.
+            this_thread::sleep_for(chrono::milliseconds(500));
+            //  this_thread::yield();
         }
-      }
-      lock_guard.unlock();
-   full_cond_var.notify_all(); // some space is there. producer please produce.
-   //this_thread::sleep_for(chrono::milliseconds(500));
-   }
-    //full_cond_var.notify_all(); // some space is there. producer please produce. 
-      this_thread::sleep_for(chrono::milliseconds(500));
-    //  this_thread::yield();
-     }
-   }
+    }
 };
 
 int main()
 {
-   vector<thread> thread_list;
-   Producer producer;
-   Consumer consumer;
+    vector<thread> thread_list;
+    Producer producer;
+    Consumer consumer;
 
-   for (int i=0;i<num_of_producers;i++) {
-       thread_list.push_back(thread(&Producer::thread_run, &producer, i));
-  }
+    for (int i = 0; i < num_of_producers; i++)
+    {
+        thread_list.push_back(thread(&Producer::thread_run, &producer, i));
+    }
 
-   for (int i=0;i<num_of_consumers;i++) {
-       thread_list.push_back(thread(&Consumer::thread_run, &consumer, i));
-  }
+    for (int i = 0; i < num_of_consumers; i++)
+    {
+        thread_list.push_back(thread(&Consumer::thread_run, &consumer, i));
+    }
 
-   for(thread& thread_iter: thread_list)  {
-       thread_iter.join();
-   }
-
+    for (thread &thread_iter : thread_list)
+    {
+        thread_iter.join();
+    }
 }
